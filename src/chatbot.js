a505cb8bfe21bc44ed84adf783f7f2ebdedea024
user.name 'Ankit Prashar <user.email 'ankitprashar1554@gmail.com>
2026-07-08 21:32:15 +0530

    code Change

--- src/chatbot.js
+++ src/chatbot.js
@@ -1,24 +1,45 @@
 
+// import Groq from "groq-sdk";
+
+// const groq = new Groq({
+ 
+// });
+
+// export async function sendmsg(message) {
+//   const chat = await groq.chat.completions.create({
+//     model: "openai/gpt-oss-20b",
+//     messages: [
+//       {
+//         role: "user",
+//         content: message,
+//       },
+//     ],
+//     max_tokens: 256,
+//     temperature: 0.7,
+//   });
+
+//   return chat.choices[0].message.content;
+
+//}
+
 import Groq from "groq-sdk";
-
 const groq = new Groq({
-  apiKey: process.env.REACT_APP_GROQ_API_KEY,
+   apiKey: process.env.REACT_APP_GROQ_API_KEY,
   dangerouslyAllowBrowser: true,
-});
 
-export async function sendmsg(message) {
-  const chat = await groq.chat.completions.create({
+}
+
+);
+ export async function main() {
+  const completion = await groq.chat.completions.create({
     model: "openai/gpt-oss-20b",
     messages: [
       {
         role: "user",
-        content: message,
+        content: "Explain why fast inference is critical for reasoning models",
       },
     ],
-    max_tokens: 256,
-    temperature: 0.7,
   });
-
-  return chat.choices[0].message.content;
-
+  console.log(completion.choices[0]?.message?.content);
 }
+main().catch(console.error);
